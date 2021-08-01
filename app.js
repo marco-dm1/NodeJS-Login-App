8d8bd5afd580c561cb1e4900fffdee6fc081ebe3
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-01 14:13:39 -0400

    Server setup for all pages

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const server = http.createServer(function(request, response){
             response.write(data);
             response.end();
         })
-        //response.write(indexPage);
     }else if(request.url == "/my/profile"){
         fs.readFile("profile.html", function(err, data){
             response.setHeader("Content-Type", "text/html");
@@ -19,8 +18,28 @@ const server = http.createServer(function(request, response){
             response.write(data);
             response.end();
         })
+    }else if(request.url == "/my/settings"){
+        fs.readFile("settings.html", function(err, data){
+            response.setHeader("Content-Type", "text/html");
+            if(err){data = "An unknown error occured."; console.log(err)};
+            response.write(data);
+            response.end();
+        })
+    }else if(request.url == "/login"){
+        fs.readFile("login.html", function(err, data){
+            response.setHeader("Content-Type", "text/html");
+            if(err){data = "An unknown error occured."; console.log(err)};
+            response.write(data);
+            response.end();
+        })
+    }else if(request.url == "/register"){
+        fs.readFile("register.html", function(err, data){
+            response.setHeader("Content-Type", "text/html");
+            if(err){data = "An unknown error occured."; console.log(err)};
+            response.write(data);
+            response.end();
+        })
     }
-    //response.setHeader("Content-Type", "text/html");
 })
 
 const port = process.env.PORT || 3000; // Look for a environment variable "PORT" or default to port 3000
