26123f724ff1985f230183fbe4f9bde5a4ee60de
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-01 11:37:18 -0400

    Addition of profile and index pages

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer(function(request, response){
-    console.log(request.url);
-    response.write("hello!");
-    response.end();
+    console.log("Received request at: " + request.url);
+
+    if(request.url == "/" || request.url == "/index"){
+        fs.readFile("index.html", function(err, data){
+            response.setHeader("Content-Type", "text/html");
+            if(err){data = "An unknown error occured."; console.log(err)};
+            response.write(data);
+            response.end();
+        })
+        //response.write(indexPage);
+    }else if(request.url == "/my/profile"){
+        fs.readFile("profile.html", function(err, data){
+            response.setHeader("Content-Type", "text/html");
+            if(err){data = "An unknown error occured."; console.log(err)};
+            response.write(data);
+            response.end();
+        })
+    }
+    //response.setHeader("Content-Type", "text/html");
 })
 
 const port = process.env.PORT || 3000; // Look for a environment variable "PORT" or default to port 3000
