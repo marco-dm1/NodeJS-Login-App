bf94f0f3631613ed014d31563d99efbb1233fb4a
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-01 14:19:49 -0400

    Login/register tweaks, register.js initial commit

---
+++ register.js
@@ -0,0 +1,11 @@
+const accountName = document.getElementById("accountName");
+const accountPassword = document.getElementById("accountPassword");
+const accountPasswordConfirm = document.getElementById("accountPassword");
+
+function register(){
+    if(accountName.value != "" && accountPassword.value != "" && accountPasswordConfirm.value != ""){
+        if(accountPasswordConfirm.value == accountPassword.value){
+            
+        }
+    }
+}
