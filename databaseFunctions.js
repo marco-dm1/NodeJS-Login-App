1be8f1f85b5a76b40b172f8c90874a28111723f5
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-03 23:36:54 -0400

    Comment explanations and general cleanup

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -1,3 +1,5 @@
+// Script that handles all database functions in the app
+
 const mongoDB = require("mongodb").MongoClient;
 const bcrypt = require("bcrypt");
 var liveDatabase;
@@ -5,59 +7,61 @@ var liveDatabase;
 mongoDB.connect("mongodb://localhost:27017/LoginData", function(err, db){
     if(err){console.log("An error occured when connecting to the database."); throw err;}
     liveDatabase = db.db("LoginDatabase");
+    // Link to the MongoDB database and save it for later use
     console.log("Connected to the MongoDB server.");
 })
 
-function searchForAccount(sendExtraData, nameInput, passwordInput, sessionInput){
-    if(sendExtraData == true){
-        liveDatabase.collection("accounts").findOne({session: sessionInput}, function(err, data){
-            if(err){throw err};
-            console.log("data", data);
-        })
-    }else{
-        return liveDatabase.collection("accounts").findOne({name: nameInput}, function(err, data){
-            if(err){console.log(err); return false;};
-            console.log("data", data);
-            bcrypt.compare(passwordInput, data["password"], function(bcryptErr, result) {
-                // Compare the inputed password to the password hash to see if its the right password.
-                if(bcryptErr){console.log(bcryptErr); return false;};
+function loginAccountSearch(nameInput){
+    // Query database using name to get the password hash to compare it with inputted password
+    return liveDatabase.collection("accounts").find({name: nameInput}, {projection:{ _id: 0, registration_time: 0}}).toArray().then(function(data){
+        return data[0];
+    }).catch(function(){
+        return false;
+    })
+}
 
-                console.log("compare result:", result);
-                if(result == true){
-                    // Password is correct since hash matches
-                    return true;
-                }else{
-                    return false;
-                }
-            });
-        })
-    }
+function searchForAccount(nameInput){
+    // Query database to determine if an account already exists, return any data that is found
+    return liveDatabase.collection("accounts").find({name: nameInput}, {projection:{ _id: 0 }}).toArray().then(function(data){
+        return data[0];
+    }).catch(function(err){
+        throw err;
+    })
+}
+
+function getAccountData(token){
+    // Query database for account data other than login/password so we can populate client's webpages with data
+    return liveDatabase.collection("accounts").find({session: token}, {projection: { _id: 0, password: 0 }}).toArray().then(function(data){
+        return data[0];
+    }).catch(function(){
+        return false;
+    })
 }
 
 function getTime(){
+    // Get the current date and time and return it in a "useable" format
     let currentTime = new Date();
     let returnData = String(currentTime.getMonth() + 1) + "/" + String(currentTime.getDate()) + "/" + String(currentTime.getFullYear()) + " - " + String(currentTime.getHours()) + ":" + String(currentTime.getMinutes()) + ":" + String(currentTime.getSeconds())
     return returnData;
 }
 
 function updateToken(targetName, newSessionToken){
-    liveDatabase.collection("accounts").UpdateOne({name: targetName}, {$set: {session: newSessionToken}}, function(err, response){
-        if(err){throw err};
-    })
+    // Tell database to update a specific document's token to facilitate new session token usage
+    liveDatabase.collection("accounts").updateOne({name: targetName}, { $set: {session: newSessionToken}}, function(err, res) {
+        if (err){throw err};
+    });
 }
 
-function registerAccount(nameInput, passwordInput, currentSession){
-    bcrypt.hash(passwordInput, 5, function(err, hash) {
-        // Store hash in your password DB.
-        if(err){throw err};
-        let currentTime = getTime()
-        liveDatabase.collection("accounts").insertOne({name: nameInput, password: hash, session: currentSession, registration_time: currentTime}, function(err2, res){
-            if(err2){throw err2};
-            console.log("inserted bob");
-        })
-    });
+function registerAccount(nameInput, passwordHash, currentSession){
+    // Create a new database document with specified information so that the user can use and log into their account
+    let currentTime = getTime()
+    liveDatabase.collection("accounts").insertOne({name: nameInput, password: passwordHash, session: currentSession, registration_time: currentTime}, function(err2, res){
+        console.log("New account was inserted");
+    })
 }
 
 module.exports.searchForAccount = searchForAccount;
 module.exports.registerAccount = registerAccount;
 module.exports.updateToken = updateToken;
+module.exports.getAccountData = getAccountData;
+module.exports.loginAccountSearch = loginAccountSearch;
